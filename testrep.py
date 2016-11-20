dff28cc774ee87df7c3687c48d61d4c6deff88a5
pathmann <thomas.pathmann@googlemail.com>
2016-11-21 00:39:04 +0100

    add some test data

---
+++ testrep.py
@@ -0,0 +1,14 @@
+from ts3plugin import ts3plugin
+
+class testscript(ts3plugin):
+    name = "testscript"
+    apiVersion = 21
+    version = "1.0.0"
+    author = ""
+    description = ""
+    requestAutoload = False
+    offersConfigure = False
+    commandKeyword = ""
+    infoTitle = None
+    menuItems = []
+    hotkeys = []
